a778406a98df1636d244c63efdf3962a26ec36f9
Threxos <threxos.48@gmail.com>
2023-01-11 15:38:42 +0100

    initial-pages-reduxsetup-hookfromredux-registeruser

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      My App
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/register" element={<Register />}></Route>
+          </Routes>
+        </div>
+        <div>{/*<ToastContainer />*/}</div>
+      </Router>
+    </>
   );
 }
 
